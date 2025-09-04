d6b410eff07515ba233be7fb011e664360d6db36
Shakirat Akanji <akanjishakirato@gmail.com>
2025-09-04 14:37:13 +0100

    Update padding of NewsletterForm on Tablet View

--- src/components/NewsletterForm.jsx
+++ src/components/NewsletterForm.jsx
@@ -10,14 +10,14 @@ export default function NewsletterForm({
   hasSubmitted,
 }) {
   return (
-    <section className="flex w-full md:w-[38rem] lg:w-[56.5rem] md:h-[54.875rem] lg:h-[40.063rem] md:p-[2.5rem] h-dvh md:rounded-[2.25rem] md:bg-white md:shadow-[0_15px_60px_0_rgba(0,0,0,0.25)]">
+    <section className="flex w-full md:w-[38rem] lg:w-[56.5rem] md:h-[54.875rem] lg:h-[40.063rem] md:p-[2rem] h-dvh md:rounded-[2.25rem] md:bg-white md:shadow-[0_15px_60px_0_rgba(0,0,0,0.25)]">
       <div className="flex flex-col lg:flex-row-reverse w-full items-center lg:items-center lg:justify-center gap-[2.5rem] ">
         <img
           src={Illustration}
           alt="Newsletter Illustration"
           className="w-full lg:w-[25rem] h-[17.75rem] md:h-[22.39288rem] lg:h-full md:rounded-[1rem] rounded-b-[1rem] object-cover "
         />
-        <div className="flex flex-col items-start gap-[2.5em] md:gap-[1.5rem] lg:gap-[2rem] md:self-stretch lg:h-full lg:justify-center px-[1.5rem] lg:px-0">
+        <div className="flex flex-col items-start gap-[2.5em] md:gap-[1.5rem] lg:gap-[2rem] md:self-stretch lg:h-full lg:justify-center px-[1.5rem] lg:px-0 md:px-0">
           <div className="flex flex-col items-start gap-[1.5rem] lg:gap-[2rem]">
             <h1 className="text-[2.5rem] md:text-[3.5rem] font-bold leading-[100%] text-blue-800">
               Stay updated!
